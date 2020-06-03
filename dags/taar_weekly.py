e27ae70d3685b47a048e05683c2f74c15d9c28f9
Victor Ng <victor@crankycoder.com>
2020-06-03 13:36:33 -0400

    Added TAAR BigTable load for user profile

--- dags/taar_weekly.py
+++ dags/taar_weekly.py
@@ -5,6 +5,8 @@ from airflow import DAG
 from airflow.contrib.hooks.aws_hook import AwsHook
 from datetime import datetime, timedelta
 from airflow.operators.subdag_operator import SubDagOperator
+
+from operators.gcp_container_operator import GKEPodOperator  # noqa
 from utils.dataproc import moz_dataproc_pyspark_runner
 
 taar_aws_conn_id = "airflow_taar_rw_s3"
@@ -13,6 +15,13 @@ taar_aws_access_key, taar_aws_secret_key, session = AwsHook(
 ).get_credentials()
 taar_ensemble_cluster_name = "dataproc-taar-ensemble"
 taar_gcpdataproc_conn_id = "google_cloud_airflow_dataproc"
+
+taar_bigquery_svc_conn_id = "google_cloud_cfr_svc"
+
+TAAR_PROFILE_PROJECT_ID = "cfr-personalization-experiment"
+
+TAAR_BIGTABLE_IMAGE = f"gcr.io/{TAAR_PROFILE_PROJECT_ID}/taar_gcp_etl:0.3"
+
 
 default_args_weekly = {
     "owner": "vng@mozilla.com",
@@ -29,6 +38,116 @@ default_args_weekly = {
 taar_weekly = DAG(
     "taar_weekly", default_args=default_args_weekly, schedule_interval="@weekly"
 )
+
+
+def wipe_args():
+    return [
+        "gsutil",
+        "-m",
+        "rm",
+        "gs://taar_profile_dump/*",
+    ]
+
+
+wipe_gcs_bucket = GKEPodOperator(
+    task_id="wipe_taar_gcs_bucket",
+    gcp_conn_id=taar_bigquery_svc_conn_id,
+    project_id=TAAR_PROFILE_PROJECT_ID,
+    location="us-central1-a",
+    cluster_name="bq-load-gke-1",
+    name="wipe_taar_gcs_bucket",
+    namespace="default",
+    image="google/cloud-sdk:242.0.0-alpine",
+    arguments=wipe_args(),
+    dag=taar_weekly,
+)
+
+dump_bq_to_tmp_table = GKEPodOperator(
+    task_id="taar_profile_bqdump",
+    name="taar-profile-bqdump",
+    # This uses a circleci built docker image from github.com/mozilla/taar_gcp_etl
+    image=TAAR_BIGTABLE_IMAGE,
+    project_id=TAAR_PROFILE_PROJECT_ID,
+    owner="vng@mozilla.com",
+    email=["vng@mozilla.com",],
+    gcp_conn_id=taar_bigquery_svc_conn_id,
+    arguments=[
+        "-m",
+        "taar_etl.taar_profile_bigtable",
+        "--iso-date={{ ds_nodash }}",
+        "--fill-table",
+    ],
+    dag=taar_weekly,
+)
+
+extract_bq_tmp_to_gcs_avro = GKEPodOperator(
+    task_id="taar_profile_bqdump",
+    name="taar-profile-bqdump",
+    # This uses a circleci built docker image from github.com/mozilla/taar_gcp_etl
+    image=TAAR_BIGTABLE_IMAGE,
+    project_id=TAAR_PROFILE_PROJECT_ID,
+    owner="vng@mozilla.com",
+    email=["vng@mozilla.com",],
+    gcp_conn_id=taar_bigquery_svc_conn_id,
+    arguments=[
+        "-m",
+        "taar_etl.taar_profile_bigtable",
+        "--iso-date={{ ds_nodash }}",
+        "--import-bigtable",
+    ],
+    dag=taar_weekly,
+)
+
+dataflow_import_avro_to_bigtable = GKEPodOperator(
+    task_id="taar_profile_bqdump",
+    name="taar-profile-bqdump",
+    # This uses a circleci built docker image from github.com/mozilla/taar_gcp_etl
+    image=TAAR_BIGTABLE_IMAGE,
+    project_id=TAAR_PROFILE_PROJECT_ID,
+    owner="vng@mozilla.com",
+    email=["vng@mozilla.com",],
+    gcp_conn_id=taar_bigquery_svc_conn_id,
+    arguments=[
+        "-m",
+        "taar_etl.taar_profile_bigtable",
+        "--iso-date={{ ds_nodash }}",
+        "--extract-avro",
+    ],
+    dag=taar_weekly,
+)
+
+wipe_gcs_bucket_cleanup = GKEPodOperator(
+    task_id="wipe_taar_gcs_bucket_cleanup",
+    gcp_conn_id=taar_bigquery_svc_conn_id,
+    project_id=TAAR_PROFILE_PROJECT_ID,
+    location="us-central1-a",
+    cluster_name="bq-load-gke-1",
+    name="wipe_taar_gcs_bucket",
+    namespace="default",
+    image="google/cloud-sdk:242.0.0-alpine",
+    arguments=wipe_args(),
+    dag=taar_weekly,
+)
+
+wipe_bq_tmp_table = GKEPodOperator(
+    task_id="taar_profile_wipe_bq_tmp_table",
+    name="taar-profile-wipe-bq-tmp",
+    # This uses a circleci built docker image from github.com/mozilla/taar_gcp_etl
+    image=TAAR_BIGTABLE_IMAGE,
+    project_id=TAAR_PROFILE_PROJECT_ID,
+    owner="vng@mozilla.com",
+    email=["vng@mozilla.com",],
+    gcp_conn_id=taar_bigquery_svc_conn_id,
+    arguments=[
+        "-m",
+        "taar_etl.taar_profile_bigtable",
+        "--iso-date={{ ds_nodash }}",
+        "--wipe-bg-tmp-table",
+    ],
+    dag=taar_weekly,
+)
+
+
 
 # This job should complete in approximately 30 minutes given
 # 35 x n1-standard-8 workers and 2 SSDs per node.
@@ -77,3 +196,13 @@ taar_ensemble = SubDagOperator(
     ),
     dag=taar_weekly,
 )
+
+
+
+wipe_gcs_bucket >> dump_bq_to_tmp_table
+dump_bq_to_tmp_table >> extract_bq_tmp_to_gcs_avro
+extract_bq_tmp_to_gcs_avro >> dataflow_import_avro_to_bigtable
+dataflow_import_avro_to_bigtable >> wipe_gcs_bucket_cleanup
+wipe_gcs_bucket_cleanup >> wipe_bq_tmp_table
+wipe_bq_tmp_table >> taar_ensemble
+
